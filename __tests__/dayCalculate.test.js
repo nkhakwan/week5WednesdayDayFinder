161190dc7436d5dad56e65e3b1135d894b9c065a
nkhakwan <post_khan@yahoo.com>
2020-04-16 12:00:23 -0700

    tested as far out as 2064 April and working. Test added

--- __tests__/dayCalculate.test.js
+++ __tests__/dayCalculate.test.js
@@ -12,6 +12,11 @@ describe('DayCalculate', () => {
     expect(dayCalculate.dayFinder()).toEqual("Teusday");
   });
 
+  test('should test whether our number of days are correct', () => { 
+    dayCalculate = new DayCalculate(2064,4,17);
+    expect(dayCalculate.dayFinder()).toEqual("Thursday");
+  });  
+
   test('should test what numerical day of the year a given date is', () => { 
     dayCalculate = new DayCalculate(2025,11,15);
     expect(dayCalculate.numberOfDays()).toEqual(319); //319
